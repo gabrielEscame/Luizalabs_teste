2d836c33c60baad9cd19f7185ef4284dfd169d1d
Gabriel Escame <gabriel.escame@gmail.com>
2019-12-19 22:36:23 -0200

    button-component-onclick-method

--- src/App.js
+++ src/App.js
@@ -1,31 +1,47 @@
-import React, {useState, useEffect, useCallback} from 'react';
+import React, {useState, useCallback} from 'react';
 import api from './service/';
 import Search from './components/Search';
 import Button from './components/Button'
 
 function App(props) {
   const [adress, setAdress] = useState([]);
-  const [value, setValue] = useState('');
-
-  useEffect(()=> {
-    api.get('03410030/json/?callback=zipCode')
-     .then(response => setAdress(response.data))
-     .catch(err => console.log(err)) 
-  }, [])
+  const [value, setValue] = useState();
+  const [alert, setAlert] = useState('');
 
   const onChange = useCallback((e) => {
-    let {value} = e.target
+    e.preventDefault();
+    let {value} = e.target;
     const filter = /^\d{1,8}$/;
     if(value === '' || filter.test(value)){
       setValue(e.target.value)
     }
   }, [])
 
-  console.log(adress);  
+  const onClick = useCallback(() => {
+    if(!value){
+      setAdress([]);
+      setAlert('insert zipcode');
+    } else {
+      api.get(`${value}/json/?callback=zipCode`)
+      .then(response => {
+        if(response.data.erro){
+          setAlert('not found...')
+        } else{
+          setAlert('');
+          setAdress(response.data)
+        }
+      })
+      .catch(err => console.log(err)) 
+    }
+  })
+
+  console.log(adress); 
+  console.log(alert) 
   return (
     <>
     <Search placeholder='search a zipCode' value={value} type='text' method={onChange}/> 
-    <Button label='Search' onClick={() => console.log('oi')} />
+    <Button label='Search' onClick={() => onClick()} />
+    <p>{alert}</p>
     <div className="App">
       {adress.logradouro}
     </div>
